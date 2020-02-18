31d268ac89dffd18afc49ea4f5e381c09bb5285d
b216014 <b216014@iiit-bh.ac.in>
2020-02-18 14:03:01 +0530

    Added the functionality to display the records in the bollywood genre.

--- src/components/Genres/BollwoodGenre/index.js
+++ src/components/Genres/BollwoodGenre/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import propTypes from 'prop-types';
+import { FaHeart } from 'react-icons/fa';
 import getRecord from '../../getRecords';
 import useRecords from '../../../hooks/useRecords';
+import './index.css';
 
 function BollywoodGenre(props) {
   const { testId } = props;
@@ -11,8 +13,21 @@ function BollywoodGenre(props) {
   console.log(bollywoodArr);
   return (
     <div data-testid={testId}>
-      <div>bollywood</div>
-      <div />
+      <div className="rock">bollywood</div>
+      <br />
+      <div className="records">
+        {
+           bollywoodArr.map((record) => (
+             <div className="rowData">
+               <img src={record.albumArtUrl} height="300" width="300" />
+               <FaHeart />
+               <div>{record.name}</div>
+               <div>{record.artists[0]}</div>
+               <br />
+             </div>
+           ))
+          }
+      </div>
     </div>
   );
 }
