780880e1bfc1020850b88d93fce4a9aa33998621
b216014 <b216014@iiit-bh.ac.in>
2020-02-18 14:01:08 +0530

    Added the functionality to show the country genres records.

--- src/components/Genres/CountryGenre/index.js
+++ src/components/Genres/CountryGenre/index.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
 import propTypes from 'prop-types';
+import { FaHeart } from 'react-icons/fa';
 import getRecord from '../../getRecords';
 import useRecords from '../../../hooks/useRecords';
 
@@ -11,8 +12,21 @@ function CountryGenre(props) {
   console.log(countryArr);
   return (
     <div data-testid={testId}>
-      <div>country</div>
-      <div />
+      <div className="rock">country</div>
+      <br />
+      <div className="records">
+        {
+           countryArr.map((record) => (
+             <div className="rowData">
+               <img src={record.albumArtUrl} height="300" width="300" />
+               <FaHeart />
+               <div>{record.name}</div>
+               <div>{record.artists[0]}</div>
+               <br />
+             </div>
+           ))
+          }
+      </div>
     </div>
   );
 }
